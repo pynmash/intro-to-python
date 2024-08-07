25a5652c885a8777f147fbee5b50858f7fcedf00
Dan Toms <danjtoms@protonmail.com>
2024-08-08 00:10:30 +0100

    Completed comparison, logic and lists

--- 027_comparison.py
+++ 027_comparison.py
@@ -39,8 +39,7 @@ print("Function: a_is_less_than_b")
 
 def a_is_less_than_b(a, b):
   # Uncomment this next line and replace ?? with the right operator
-  # return a ?? b
-  pass
+  return a < b
 
 check_that_these_are_equal(
   a_is_less_than_b(1, 2),
@@ -63,7 +62,7 @@ print("")
 print("Function: a_is_greater_than_b")
 
 def a_is_greater_than_b(a, b):
-  # return a ?? b
+  return a > b
   pass
 
 check_that_these_are_equal(
@@ -87,7 +86,7 @@ print("")
 print("Function: a_is_less_than_or_equal_to_b")
 
 def a_is_less_than_or_equal_to_b(a, b):
-  # return a ?? b
+  return a <= b
   pass
 
 check_that_these_are_equal(
@@ -111,7 +110,7 @@ print("")
 print("Function: a_is_greater_than_or_equal_to_b")
 
 def a_is_greater_than_or_equal_to_b(a, b):
-  # return a ?? b
+  return a >= b
   pass
 
 check_that_these_are_equal(
@@ -135,7 +134,7 @@ print("")
 print("Function: a_is_not_equal_to_b")
 
 def a_is_not_equal_to_b(a, b):
-  # return a ?? b
+  return a != b
   pass
 
 check_that_these_are_equal(
@@ -161,7 +160,7 @@ print("Function: a_is_within_b")
 # May be a little tricky — search for
 # "python check if string contains substring"
 def a_is_within_b(a, b):
-  # return a ?? b
+  return a in b
   pass
 
 check_that_these_are_equal(
--- 029_lists.py
+++ 029_lists.py
@@ -25,7 +25,7 @@ my_friends = ["Victoria", "Mel", "Melanie", "Emma"]
 
 # @TASK Try making your own here:
 
-your_list = ...
+your_list = ["blah", 23.4, ['a','b','c'], 3,2,1]
 print(your_list)
 
 # @TASK and print it out by running:
